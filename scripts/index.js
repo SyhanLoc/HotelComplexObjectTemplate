17ff6561851d1632791ee0d33497143adb59b172
Syhan Loc <syhanloc@gmail.com>
2017-11-20 18:36:17 +0000

    display room info

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,33 @@ var hotel = {
     name: "CareerDevs Innt"
 }
 
+function displayDetails(room) {
+    console.log("hello");
+    console.log(room);
+    document.getElementById("price").innerHTML = hotel.rooms[room].price;
+    document.getElementById("available").innerHTML = hotel.rooms[room].available;
+    
+}
+
+// console.log(document.getElementById("label").nodeName);
+
+for (var i = 0; i < hotel.rooms.length; i++) {
+    console.log("start");
+    var radioBtn = document.createElement("INPUT");
+    var radioLbl = document.createElement("LABEL");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "rooms");
+    radioBtn.setAttribute("value", i);
+    radioBtn.setAttribute("id", "room" + i);
+    radioBtn.setAttribute("onclick", "displayDetails(" + i + ")"); //<---interpolition
+    // radioBtn.onclick = displayDetails(i);
+    radioLbl.innerHTML = hotel.rooms[i].name;
+    console.log("stop");
+    
+    document.getElementById("radialSection").appendChild(radioBtn);
+    document.getElementById("radialSection").appendChild(radioLbl);
+}
+//display rooms as radio options
+
+//on form submission confirm radio was selected
+//and checkbox checked
